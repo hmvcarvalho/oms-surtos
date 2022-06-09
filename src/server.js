5181d118b60dffb0528dab288d0a701774e4ef4a
Pedro Dias <Dias.PedroRibeiro@gmail.com>
2022-06-09 16:36:44 +0100

    Project init

---
+++ src/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+let { connect, Promise, connection } = require('mongoose');
+require('dotenv').config();
+
+const port = process.env.PORT || 3000;
+
+connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+Promise = global.Promise;
+
+app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+connection.on('error', console.error.bind(console, 'connection error:'));
+
+connection.once('open', () => {
+    console.log('Connected to MongoDB');
+
+    app.listen(port, () => {
+        console.log(`Server listening on port http://localhost:${port}`);
+    });
+});
