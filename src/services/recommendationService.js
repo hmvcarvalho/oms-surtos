23177e6561c83e27a112b15369e11e34b76dfac1
Pedro Dias <Dias.PedroRibeiro@gmail.com>
2022-06-13 22:40:31 +0100

    Add route paises/{código_país}/recomendacoes

--- src/services/recommendationService.js
+++ src/services/recommendationService.js
@@ -1,4 +1,5 @@
 const { RecommendationModel } = require('../models/recommendationModel');
+const CountryModel = require('../models/countryModel');
 
 function listRecomendations() {
     return RecommendationModel.find().then((recommendations) => {
@@ -28,10 +29,19 @@ function deleteRecomendation(id) {
     return RecommendationModel.deleteOne(id);
 }
 
+function listRecomemendationsByCountry(countryID) {
+    return CountryModel.findOne({ code: countryID }).then(({ zoneCode }) => {
+        return RecommendationModel.find({ geoZoneCode: zoneCode }).then((recommendations) => {
+            return recommendations;
+        });
+    });
+}
+
 module.exports = {
     createRecomendation,
     listRecomendations,
     getRecomendation,
     updateRecomendation,
     deleteRecomendation,
+    listRecomemendationsByCountry,
 };
