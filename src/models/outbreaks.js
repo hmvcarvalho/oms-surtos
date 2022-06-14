7a337b121185f248fa9bcf151d9528b246906421
HelenaCarvalho <hmvcarvalho@gmail.com>
2022-06-14 16:34:42 +0100

    add route api/country/{code}/outbreaks

--- src/models/outbreaks.js
+++ src/models/outbreaks.js
@@ -3,10 +3,18 @@ const Schema = mongoose.Schema;
 
 const outbreakSchema = new Schema({
     id: Schema.Types.ObjectId,
-    virusCode: { type: String, required: true },
-    geoCode: { type: String, required: true },
-    detDate: { type: Date, required: true },        // VALIDAR detDate como anterior
-    endDate: { type: Date },                        // a endDate
+    virusCode: {
+        type: Schema.Types.ObjectId,
+        ref: 'Virus',
+        required: true,
+    },
+    geoCode: {
+        type: Schema.Types.ObjectId,
+        ref: 'GeoZone',
+        required: true,
+    },
+    detDate: { type: Date, required: true }, // VALIDAR detDate como anterior
+    endDate: { type: Date }, // a endDate
 });
 
 module.exports = mongoose.model('Outbreaks', outbreakSchema);
