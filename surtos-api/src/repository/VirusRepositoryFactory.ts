9d1854195556d91d9ba162936cf1d8dadff158df
andre faria <andrefaria@upskill.pt>
2022-06-23 15:05:48 +0100

    implementation of virusRoutes with get & post

---
+++ surtos-api/src/repository/VirusRepositoryFactory.ts
@@ -0,0 +1,12 @@
+import IVirusRepository from './IVirusRepository';
+import { datasource } from '../app';
+import { MongoVirusRepository } from './mongo/MongoVirusRepository';
+
+export class VirusRepositoryFactory {
+    public static virusRepository(): IVirusRepository {
+        switch (datasource.repository) {
+            default:
+                return new MongoVirusRepository();
+        }
+    }
+}
