2ffda86d1b3a25ec28f68b8f7f938fca41f2d0aa
Pedro Dias <Dias.PedroRibeiro@gmail.com>
2022-06-27 15:17:29 +0100

    Update doctorSchema.ts

--- diagnostico-api/src/persistence/schemas/doctorSchema.ts
+++ diagnostico-api/src/persistence/schemas/doctorSchema.ts
@@ -1,6 +1,8 @@
 import { Schema } from 'mongoose';
 import { User, options } from './userSchema';
 
-const Doctor = User.discriminator('Doctor', new Schema({ doctorNumber: { type: String, index: true } }, options));
+const doctorSchema = new Schema({ doctorNumber: { type: String, index: true } }, options);
+
+const Doctor = User.discriminator('Doctor', doctorSchema);
 
 export { Doctor };
