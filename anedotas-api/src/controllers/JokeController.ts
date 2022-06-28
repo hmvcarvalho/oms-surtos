b8bd7a0db85b8697b9fabccc1e88e332ee357832
Pedro Dias <Dias.PedroRibeiro@gmail.com>
2022-06-28 16:37:19 +0100

    Update JokeController.ts

--- anedotas-api/src/controllers/JokeController.ts
+++ anedotas-api/src/controllers/JokeController.ts
@@ -11,9 +11,9 @@ class JokeController implements IJokeController {
         try {
             const jokeDto: IJokeDTO = req.body;
             const newJoke = await this.jokeService.createJoke(jokeDto);
-            res.status(200).send('Joke created with sucess');
-        } catch {
-            res.status(400).json(JSON.stringify(Error));
+            res.status(201).send(newJoke);
+        } catch (err) {
+            res.status(400).json(JSON.stringify(err));
         }
     };
 
@@ -22,7 +22,7 @@ class JokeController implements IJokeController {
             const jokeList = await this.jokeService.getAllJokes();
             res.status(200).send(jokeList);
         } catch (error) {
-            res.status(400).json(JSON.stringify(Error));
+            res.status(400).json(JSON.stringify(error));
         }
     };
 
@@ -30,7 +30,7 @@ class JokeController implements IJokeController {
         try {
             res.status(200).send(await this.jokeService.getJokeById(req.params.id));
         } catch (error) {
-            res.status(400).json(JSON.stringify(Error));
+            res.status(400).json(JSON.stringify(error));
         }
     };
 
@@ -47,8 +47,8 @@ class JokeController implements IJokeController {
     put = async (req: Request, res: Response, next: NextFunction) => {
         try {
             const jokeDTO: IJokeDTO = req.body;
-            await this.jokeService.updateJoke(req.params.id, jokeDTO);
-            res.status(200).send(`Joke with id : ${req.params.id} updated with success`);
+            const jokeUpdated = await this.jokeService.updateJoke(req.params.id, jokeDTO);
+            res.status(200).send(jokeUpdated);
         } catch (error) {
             res.status(400).json(JSON.stringify(error));
         }
